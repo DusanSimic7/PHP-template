3eb1b477b8b5f4797ce6ed81f1169f092c85a85f
Dusan Simic <dusansimic202@gmail.com>
2024-01-26 13:38:21 +0100

    refatored router and other things

--- Core/functions.php
+++ Core/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+use Core\Response;
+
+
+
 function dd($value)
 {
     echo "<pre>";
@@ -13,6 +17,15 @@ function dd($value)
 function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function abort($code = 404){
+
+    http_response_code($code);
+
+    require base_path("views/{$code}.php");
+
+    die();
 }
 
 
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,6 +7,8 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
+
+
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
 ])->findOrFail();
@@ -18,3 +20,4 @@ view("/notes/show.view.php", [
     'heading' => 'Note',
     'note' => $note,
 ]);
+
