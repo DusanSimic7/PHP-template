d8eb4ac27320389aadf7dd400ce7c429348dc6b7
Dusan Simic <dusansimic202@gmail.com>
2024-01-29 21:08:51 +0100

    finished with crud operations

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -9,13 +9,21 @@
         </p>
 
         <p><?= htmlspecialchars($note['body']) ?></p>
+
+        <footer class="mt-6">
+
+        <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >Edit</a>
+
+        </footer>
+
         
-        <form class="mt-6" action="" method="POST">
+        <!-- <form class="mt-6" action="" method="POST">
 
             <input type="hidden" name="_method" value="DELETE">    
             <input type="hidden" name="id" value="<?= $note['id'] ?>">    
             <button class="text-sm text-red-500">Delete</button>
-        </form>    
+        </form>     -->
 
     </div>
 </main>
